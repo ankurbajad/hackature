b2ff21dcb3e03e1a390bf4d036b3444225095eb8
Jan De Poorter <jan@defv.be>
2014-06-10 18:04:55 +0200

    Add email to candidature

--- app/admin/candidature.rb
+++ app/admin/candidature.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register Candidature do
   index do
     column :name
+    column :email
     column(:motivation) {|model| truncate(model.motivation) }
     column(:resume) {|model| link_to "download", model.resume.url }
     column :created_at
--- app/models/candidature.rb
+++ app/models/candidature.rb
@@ -1,5 +1,5 @@
 class Candidature < ActiveRecord::Base
-  validates_presence_of :name, :motivation, :resume
+  validates_presence_of :name, :motivation, :email
   serialize :steps
 
   mount_uploader :resume, ResumeUploader
