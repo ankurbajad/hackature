d769dac0dcd2540ffd3170a092ceb727b25d6844
Jan De Poorter <jan@defv.be>
2014-06-10 15:31:40 +0200

    Update unicorn config for Rails 4.1

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -18,19 +18,20 @@ before_exec do |_|
 end
 
 before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
-
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-    end
-  end
 end
 
 after_fork do |server, worker|
-  ActiveRecord::Base.establish_connection
-  ActiveRecord::Base.verify_active_connections!
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
 end
